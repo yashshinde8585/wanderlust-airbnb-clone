4cce0c1ebd84f1e1812f69d789049cc207b59be2
yashshinde8585 <yashshinde8585@gmail.com>
2026-01-11 22:13:22 +0530

    favicon image error fix

--- utils/ExpressError.js
+++ utils/ExpressError.js
@@ -1,7 +1,7 @@
 class ExpressError extends Error {
   constructor(message, statusCode = 500) {
     super(message);
-    this.status = statusCode;
+    this.statusCode = statusCode;
     this.message = message;
   }
 }
